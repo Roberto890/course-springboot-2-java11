e3265b2820a904df5e96400b8d07e0e0073aafed
Roberto Jesus <roberto.jesus.amaral@gmail.com>
2020-10-19 20:25:55 -0300

    Exception handling - delete

--- src/main/java/com/robertojes/projetoweb/Services/UserService.java
+++ src/main/java/com/robertojes/projetoweb/Services/UserService.java
@@ -4,10 +4,13 @@ import java.util.List;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.stereotype.Service;
 
 import com.robertojes.projetoweb.entities.User;
 import com.robertojes.projetoweb.repositories.UserRepository;
+import com.robertojes.projetoweb.services.exception.DatabaseException;
 import com.robertojes.projetoweb.services.exception.ResourceNotFoundException;
 
 // anotação pra virar componente @Component
@@ -33,7 +36,13 @@ public class UserService {
 	}
 	
 	public void delete(Long id) {
-		repository.deleteById(id);
+		try {
+			repository.deleteById(id);
+		}catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException(id);
+		}catch (DataIntegrityViolationException e) {
+			throw new DatabaseException(e.getMessage());
+		}
 	}
 	
 	public User update(Long id, User obj) {
